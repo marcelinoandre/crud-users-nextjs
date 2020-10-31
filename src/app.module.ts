57cbe89da1a044c9be6b4c25cdd34a342b45e5ee
Andre Marcelino <marcelinoandre@gmail.com>
2020-10-31 19:00:03 -0300

    adicionados logs na aplicação

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { databaseConfig } from './configs/database.config'
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { WinstonModule } from 'nest-winston'
+import { winstonConfig } from './configs/logs-winston.config'
+import { LoggerInterceptor } from './interceptors/logger.interceptor'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
-  imports: [TypeOrmModule.forRoot(databaseConfig), UsersModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    WinstonModule.forRoot(winstonConfig),
+    UsersModule,
+    AuthModule
+  ],
   controllers: [],
-  providers: []
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor
+    }
+  ]
 })
 export class AppModule {}
